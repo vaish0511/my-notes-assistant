8e9bbc685577d326618c599a23d8b77da1022bca
vaishnavisingodia <vaishnavisingodia@gmail.com>
2025-03-09 21:26:42 +0530

    all commits

---
+++ src/main/java/com/research/notes_assistant/NotesAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.research.notes_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/research/notes_assistant/NotesAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.notes_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
